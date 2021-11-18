9f907fcdf6783f996620a64b1887e9d7c4a8edf4
wangjie-zzz <1948317750@qq.com>
2021-11-18 11:38:10 +0800

    best

--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -1,13 +1,30 @@
 import { clientService } from "@/services/client-service";
 import { SysDict } from "pf-component/packages/services/model/SysDict";
 import { systemApi } from "@/constants/api/system-api";
-import { Constants } from "@/constants/constants";
 import { useNotice } from "@/components/element-plus/notice";
 import { useDict } from "pf-component/packages/util/dict-convert";
 import { authApi } from "@/constants/api/auth-api";
 import { isNull } from "pf-component/packages/util/objects-utils";
+import { Constants } from "@/constants/constants";
 
 class AuthService {
+  private static REDIRECT_URI = "http://localhost:4200/dcms/callback";
+  private static readonly AUTHORIZE_CALLBACK_PARAMS = {
+    // eslint-disable-next-line @typescript-eslint/camelcase
+    client_id: "pf-web",
+    // eslint-disable-next-line @typescript-eslint/camelcase
+    client_secret: "123456",
+    // eslint-disable-next-line @typescript-eslint/camelcase
+    redirect_uri: `${AuthService.REDIRECT_URI}`
+  };
+  private static readonly AUTHORIZE_CODE_PARAMS = {
+    // eslint-disable-next-line @typescript-eslint/camelcase
+    client_id: "pf-web",
+    // eslint-disable-next-line @typescript-eslint/camelcase
+    redirect_uri: `${AuthService.REDIRECT_URI}`,
+    // eslint-disable-next-line @typescript-eslint/camelcase
+    response_type: "code"
+  };
   private static readonly TOKEN = "access_token";
   private static readonly REFRESH_TOKEN = "refresh_token";
   private static readonly USER = "user_identity";
@@ -55,7 +72,7 @@ class AuthService {
     });
   }
   authCode(): void {
-    const href = clientService.urlQueryConvert(authApi.oauthApi.authorize.url, Constants.AUTHORIZE_CODE_PARAMS);
+    const href = clientService.urlQueryConvert(authApi.oauthApi.authorize.url, AuthService.AUTHORIZE_CODE_PARAMS);
     location.href = href;
   }
   token(params: any): Promise<boolean> {
@@ -64,7 +81,7 @@ class AuthService {
     }
     return clientService
       .general(authApi.oauthApi.callback, undefined, {
-        ...Constants.AUTHORIZE_CALLBACK_PARAMS,
+        ...AuthService.AUTHORIZE_CALLBACK_PARAMS,
         ...params
       })
       .then(res => {
@@ -79,7 +96,7 @@ class AuthService {
   }
   refreshToken(): Promise<boolean> {
     // eslint-disable-next-line @typescript-eslint/camelcase
-    const params = { ...Constants.AUTHORIZE_CALLBACK_PARAMS, refresh_token: this.getRefreshToken() };
+    const params = { ...AuthService.AUTHORIZE_CALLBACK_PARAMS, refresh_token: this.getRefreshToken() };
     return clientService.general(authApi.oauthApi.refreshToken, undefined, params).then(res => {
       authService.setCache(res.data);
       return Promise.resolve(true);
